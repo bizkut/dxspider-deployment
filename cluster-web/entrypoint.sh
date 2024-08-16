6697fd40e89a27a7a07e52d3a981ff5174f6d647
bizkut <bizkut.limau@gmail.com>
2024-08-16 11:32:51 +0800

    Test

--- cluster-web/entrypoint.sh
+++ cluster-web/entrypoint.sh
@@ -1,14 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
-sed -e 's/\("host":\)"localhost"/\1"${CLUSTERWEB_DB_CONTAINERHOSTNAME}"/' \
-    -e 's/\("user":\)"foo"/\1"${CLUSTERWEB_USER}"/' \
-    -e 's/\("passwd":\)"bar"/\1"${CLUSTERWEB_PASSWORD}"/' \
-    -e 's/\("db":\)"dxcluster"/\1"${CLUSTERWEB_DATABASE}"/' \
-    -e 's/\("mycallsign":\)"XXXXXX"/\1"${CLUSTER_CALLSIGN}"/' \
-    -e 's/\("mail":\)"foo@bar.com"/\1"${CLUSTER_SYSOP_EMAIL}"/' \
-    -e 's/\("host":\) "mysite"/\1 "${CLUSTER_DX_HOSTNAME}"/' \
-    -e 's/\("port":\) "7300"/\1 "${CLUSTER_PORT}"/' \
-    -e 's/\("user":\) "myuser"/\1 "${CLUSTER_SYSOP_CALLSIGN}"/' \
-    -e 's/\("password":\) ""/\1 "${CLUSTER_SYSOP_PASSWORD}"/' /app/cfg/config.json.template > /app/cfg/config.json
+# call signs should be all upper case
+CLUSTER_CALLSIGN=$(echo ${CLUSTER_CALLSIGN} | tr '[a-z]' '[A-Z]')
+CLUSTER_SYSOP_CALLSIGN=$(echo ${CLUSTER_SYSOP_CALLSIGN} | tr '[a-z]' '[A-Z]')
+#CLUSTERWEB_DB_CONTAINERHOSTNAME=$(echo )
+#CLUSTERWEB_USER=$(echo )
+#CLUSTERWEB_PASSWORD=$(echo )
+#CLUSTERWEB_DATABASE=$(echo )
+#CLUSTER_SYSOP_EMAIL=$(echo )
+#CLUSTER_DX_HOSTNAME=$(echo )
+#CLUSTER_PORT=$(echo )
+#CLUSTER_SYSOP_PASSWORD=$(echo )
 
+
+# [ ! -f  /app/cfg/config.json -o "${OVERWRITE_CONFIG}" = "yes" ] && \
+sed -e "s/\(\"host\":\).*$/\1\"${CLUSTERWEB_DB_CONTAINERHOSTNAME}\";/" \
+    -e "s/\(\"user\"\).*$/\1\"${CLUSTERWEB_USER}\";/" \
+    -e "s/\(\"passwd\"\).*$/\1\"${CLUSTERWEB_PASSWORD}\";/" \
+ < /app/cfg/config.json.template > /app/cfg/config.json
+
+cat /app/cfg/config.json
 python3 /app/wsgi.py
